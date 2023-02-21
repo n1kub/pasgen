ed873fc68216d3c4cc5e007cafc02e4d28a27242
v.melnikov <v.melnikov@geoscan.aero>
2023-02-21 10:22:04 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {observer} from "mobx-react-lite";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppView = observer(() => {
+  return (<div className='d-flex flex-row align-items-center justify-content-center'>
+    <p></p>
+  </div>)
+});
 
-export default App;
+export default AppView;
