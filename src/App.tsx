58deb945e094db75d6a3ea61481365e36fc88095
v.melnikov <v.melnikov@geoscan.aero>
2023-02-21 10:46:00 +0300

    Заготовка под форму

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import {observer} from "mobx-react-lite";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const AppView = observer(() => {
-  return (<div className='d-flex flex-row align-items-center justify-content-center'>
-    <p></p>
+  return (<div className='d-flex flex-row align-items-center justify-content-center  rounded-3 w-50 bg-dark mb-5 overflow-auto'>
+    <p className='bg-white my-3'></p>
   </div>)
 });
 
