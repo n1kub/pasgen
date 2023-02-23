bab5d049400f74030f66b8e8ed17384fe53b36f4
n1kub <n1kub@mail.ru>
2023-02-23 22:56:54 +0300

    added field to request

--- src/service/generate_password.ts
+++ src/service/generate_password.ts
@@ -1,10 +1,11 @@
-const generate_password = async (with_special_symbols: boolean, has_upper: boolean, password_to_modify?: string) => {
+const generate_password = async (with_special_symbols: boolean, has_upper: boolean, password_length: number, password_to_modify?: string) => {
     if (!password_to_modify) {
         password_to_modify = ""
     }
     const result = await fetch("http://localhost:8080/", {
         method: "GET",
         body: JSON.stringify({
+            password_length: password_length,
             with_special_symbols: with_special_symbols,
             has_upper: has_upper,
             password_to_modify: password_to_modify
