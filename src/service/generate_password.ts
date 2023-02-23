2245ee24eda60da42c1bab8d03a2d0852f65a9de
n1kub <n1kub@mail.ru>
2023-02-23 10:03:47 +0300

    deleted some logic that will be on server and useless here added values to request

--- src/service/generate_password.ts
+++ src/service/generate_password.ts
@@ -1,6 +1,14 @@
-const generate_password = async () => {
+const generate_password = async (with_special_symbols: boolean, has_upper: boolean, password_to_modify?: string) => {
+    if (!password_to_modify) {
+        password_to_modify = ""
+    }
     const result = await fetch("http://localhost:3030/scanning", {
         method: "GET",
+        body: JSON.stringify({
+            with_special_symbols: with_special_symbols,
+            has_upper: has_upper,
+            password_to_modify: password_to_modify
+        }),
         credentials: "include",
         headers: {"Content-Type": "application/json"},
     });
--- src/service/mobx.ts
+++ src/service/mobx.ts
@@ -1,4 +1,5 @@
-import { observable, makeObservable, action, runInAction } from "mobx";
+import {action, makeObservable, observable} from "mobx";
+import generate_password from "./generate_password";
 
 export class password {
     password_value: string;
@@ -9,11 +10,19 @@ export class password {
     constructor(password_value: string, has_upper: boolean, has_special_symbols: boolean) {
         makeObservable(this, {
             password_value: observable,
-            has_upper: observable,
-            has_special_symbols: observable,
+            retrieve_new_password: action,
+            modify_password: action
         });
         this.password_value = password_value;
         this.has_upper = has_upper;
         this.has_special_symbols = has_special_symbols;
+    }
+
+    async retrieve_new_password() {
+        this.password_value = await generate_password(this.has_upper, this.has_special_symbols);
+    }
+
+    async modify_password() {
+        this.password_value = await generate_password(this.has_upper, this.has_special_symbols, this.password_value);
     }
 }
