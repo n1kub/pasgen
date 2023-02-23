1ea4f9d47dee0098e7b1aa89cef66eca2245391a
n1kub <n1kub@mail.ru>
2023-02-23 10:10:30 +0300

    wrong server path changed

--- src/service/generate_password.ts
+++ src/service/generate_password.ts
@@ -2,7 +2,7 @@ const generate_password = async (with_special_symbols: boolean, has_upper: boole
     if (!password_to_modify) {
         password_to_modify = ""
     }
-    const result = await fetch("http://localhost:3030/scanning", {
+    const result = await fetch("http://localhost:8080/", {
         method: "GET",
         body: JSON.stringify({
             with_special_symbols: with_special_symbols,
